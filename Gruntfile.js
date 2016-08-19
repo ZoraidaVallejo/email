6cf9ef71ffe787ab8d7bbe66f0816238e01fffbc
Diego Miguel <ryu.orochi@gmail.com>
2016-08-19 15:02:08 -0500

    :memo: Q&A icon and images optimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
         // Pass data to tasks
         data: {
             port: 4000,
-            justatic_version: '20160818a',
+            justatic_version: '20160819a',
             current_year: '2016',
             current_month: '08',
 
