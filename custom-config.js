85c6570af3870b2d6a0dea87c25e3c83719a3fe9
Diego Miguel <digznav@gmail.com>
2017-01-30 18:50:24 -0600

    :memo: Prepering to new module.

--- custom-config.js
+++ custom-config.js
@@ -3,9 +3,9 @@
 module.exports = {
     conversionType: 'newsletter',
     port: 4000,
-    justatic_version: '20170125a',
-    current_year: '2017',
-    current_month: '02',
+    justatic_version: '1',
+    current_year: '0000',
+    current_month: '00',
 
     // Re-usable filesystem path variables
     paths: {
