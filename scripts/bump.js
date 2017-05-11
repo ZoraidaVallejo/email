77d78880a630df4b154e4bc4559b99fb05dcea6f
Diego Miguel <digznav@gmail.com>
2017-05-10 19:46:03 -0500

    :rotating_light: Creating test to bump version in files

--- scripts/bump.js
+++ scripts/bump.js
@@ -6,21 +6,22 @@
 
 // SYSTEM
 // ------
-const path = require('path');
 const cwd = process.cwd();
+const path = require('path');
 
 // EXTERNAL PACKAGES
 // -----------------
 const chalk = require('chalk');
 const figures = require('figures');
-// const inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const replace = require('replace-in-file');
 // const git = require('simple-git')(cwd);
 
 // CUSTOM PACKAGES
 // ---------------
 const $ = require('./helpers');
+// const bumpfiles = require('./bumpFiles');
 // const CHANGELOG = require('./changelog');
-// const BUMPFILES = require('./bumpFiles');
 
 // FILES
 // -----
@@ -30,102 +31,141 @@ const PKG = require(path.join(cwd, 'package.json'));
 // ----------------------------------------------------------------------------------------------------------
 
 var versionList = $.versionInfo(PKG.version);
-
 var targetBranch = 'diego-public';
-var overallStatus = $.checkOverallStatus()
 
-    .then((status) => {
-        let summary = status[0];
+var overallStatus = $.checkOverallStatus().then((status) => {
+    let summary = status[0];
 
-        if (summary.current !== targetBranch) {
-            console.error(
-                chalk.red([
-                    `\n${ figures.cross } ${ chalk.bold('Working on the wrong branch!') }`,
-                    `  All tags must be created on ${ chalk.bold(targetBranch) } branch. Please push your work and`,
-                    `  create a Pull Request to ${ chalk.bold(targetBranch) }. Then you can continue this process.`
-                ].join('\n'))
-            );
-
-            process.exit(1);
-        }
-
-        if (summary.files.length > 0) {
-            console.error(
-                chalk.red([
-                    `\n${ figures.cross } ${ chalk.bold('Working dirty!') }`,
-                    '  Please commit before trying again!'
-                ].join('\n'))
-            );
-
-            process.exit(1);
-        }
-
-        return summary.current;
-    })
-
-    // Catch for any errors.
-    .catch((reason) => {
+    if (summary.current !== targetBranch) {
         console.error(
-            chalk.red(`\n${ figures.cross } ${ reason }`)
+            chalk.red([
+                `\n${ figures.cross } ${ chalk.bold('Working on the wrong branch!') }`,
+                `  All tags must be created on ${ chalk.bold(targetBranch) } branch. Please push your work and`,
+                `  create a Pull Request to ${ chalk.bold(targetBranch) }. Then you can continue this process.`
+            ].join('\n'))
         );
 
         process.exit(1);
-    });
+    }
+
+    if (summary.files.length > 0) {
+        console.error(
+            chalk.red([
+                `\n${ figures.cross } ${ chalk.bold('Working dirty!') }`,
+                '  Please commit before trying again!'
+            ].join('\n'))
+        );
+
+        // process.exit(1);
+    }
+
+    return summary.current;
+
+// Catch for any errors.
+}).catch($.catchError);
 
 
-Promise.all([overallStatus])
-    .then((currentBranch) => {
-        console.log(currentBranch);
+Promise.all([overallStatus]).then((currentBranch) => {
+
+    console.log(
+        chalk.cyan(
+            `\n${ figures.info } Current version in package.json is ${ chalk.bold(versionList.current) }`
+        )
+    );
+
+    inquirer.prompt([{
+        type: 'list',
+        name: 'newVersion',
+        message: 'How would you like to bump it?',
+        choices: [
+            { value: 'nextMajor', name: `major (${ versionList.nextMajor })` },
+            { value: 'nextMinor', name: `minor (${ versionList.nextMinor })` },
+            { value: 'nextPatch', name: `patch (${ versionList.nextPatch })` },
+            { value: 'nextPreMajor', name: `pre-release major (${ versionList.nextPreMajor })` },
+            { value: 'nextPreMinor', name: `pre-release minor (${ versionList.nextPreMinor })` },
+            { value: 'nextPrePatch', name: `pre-relase patch (${ versionList.nextPrePatch })` },
+            { value: 'nextPreRelease', name: `pre-release (${ versionList.nextPreRelease })` }
+        ]
+
+    }]).then((answers) => {
+        let repalceOptions = {
+
+            files: [
+                'responsive-starter-superwide/css/scss/**/*.scss',
+                'responsive-starter-superwide/js/*.js',
+                'single-column/css/scss/**/*.scss',
+                'single-column/js/*.js',
+                'README.md'
+            ],
+
+            // Replacement to make (string or regex)
+            from: /((?:#|\/\/) Conversion Starter(?:s|) v)(\d.*)/,
+            to: `$1${ bumpVersion }`,
+
+            // Specify if empty/invalid file paths are allowed (defaults to false)
+            // If set to true these paths will fail silently and no error will be thrown.
+            allowEmptyPaths: false,
+
+            // Character encoding for reading/writing files (defaults to utf-8)
+            encoding: 'utf8'
+        };
+
+        versionList.newVersion = versionList[answers.newVersion];
+
+        // Bump Version in all files
+        return replace(repalceOptions);
+
+    // Success
+    }).then((changedFiles) => {
 
         console.log(
-            chalk.cyan(
-                `\n${ figures.info } Current version in package.json is ${ chalk.bold(versionList.current) }`
+            chalk.green(
+                `\n${ figures.tick } Version bumped in the following files:\n`
             )
         );
-    })
 
-    // Catch for any errors.
-    .catch((reason) => {
-        console.error(
-            chalk.red(`\n${ figures.cross } ${ reason }`)
+        for (let file of changedFiles) {
+            console.log(
+                chalk.green(
+                    `${ figures.arrowRight } ${ file }`
+                )
+            );
+        }
+
+        PKG.version = versionList.newVersion;
+
+        // Update Package file
+        return $.writeFileP(path.join(cwd, 'package.json'), PKG);
+
+    // Success
+    }).then((reweritePKG) => {
+        console.log(reweritePKG);
+
+        console.log(
+            chalk.green(`${ figures.arrowRight } ${ file.replace(`${ cwd }/`, '') }`)
         );
 
-        process.exit(1);
-    });
+        // BUMPFILES(versionList.newVersion).then(function(prefixedVersion) {
 
-// inquirer.prompt([{
-//     type: 'list',
-//     name: 'newVersion',
-//     message: 'How would you like to bump it?',
-//     choices: [
-//         { value: 'nextMajor', name: `major (${versionList.nextMajor})` },
-//         { value: 'nextMinor', name: `minor (${versionList.nextMinor})` },
-//         { value: 'nextPatch', name: `patch (${versionList.nextPatch})` },
-//         { value: 'nextPreMajor', name: `pre-release major (${versionList.nextPreMajor})` },
-//         { value: 'nextPreMinor', name: `pre-release minor (${versionList.nextPreMinor})` },
-//         { value: 'nextPrePatch', name: `pre-relase patch (${versionList.nextPrePatch})` },
-//         { value: 'nextPreRelease', name: `pre-release (${versionList.nextPreRelease})` }
-//     ]
-// }]).then(function(answers) {
-//     versionList = Object.assign(versionList, { newVersion: versionList[answers.newVersion] });
+        //     CHANGELOG(prefixedVersion).then(function(filesCreated) {
+        //         console.log(
+        //             chalk.green(
+        //                 `\n${ figures.tick } Changelog:\n`
+        //             )
+        //         );
+        //         console.log(filesCreated);
 
-//     BUMPFILES(versionList.newVersion).then(function(prefixedVersion) {
+        //         $.releaseGit(prefixedVersion);
 
-//         CHANGELOG(prefixedVersion).then(function(filesCreated) {
-//             console.log(
-//                 chalk.green(
-//                     `\n${figures.tick} Changelog:\n`
-//                 )
-//             );
-//             console.log(filesCreated);
+        //     }).catch(function(error) {
+        //         console.error(`\n${ error }`);
+        //     });
 
-//             $.releaseGit(prefixedVersion);
+        // }).catch(function(error) {
+        //     console.error(`\n${ error }`);
+        // });
 
-//         }).catch(function(error) {
-//             console.error(`\n${error}`);
-//         });
+    }).catch($.catchError);
 
-//     }).catch(function(error) {
-//         console.error(`\n${error}`);
-//     });
-// });
+// Catch for any errors.
+}).catch($.catchError);
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -6,7 +6,7 @@
 
 // SYSTEM MODULES
 // --------------
-// const fs = require('fs');
+const fs = require('fs');
 const cwd = process.cwd();
 // const path = require('path');
 
@@ -15,8 +15,8 @@ const cwd = process.cwd();
 const chalk = require('chalk');
 const semver = require('semver');
 const figures = require('figures');
-const git = require('simple-git')(cwd);
 // const openUrl = require('openurl');
+const git = require('simple-git')(cwd);
 
 
 // ----------------------------------------------------------------------------------------------------------
@@ -62,11 +62,16 @@ function checkOverallStatus() {
 }
 
 
-// Borrowed form version-bump-prompt
-// https://github.com/BigstickCarpet/version-bump-prompt/blob/master/lib/index.js#L50
-var versionInfo = function(version) {
-    let current = new semver(version || '0.1.0'),
-        identifier = current.prerelease[0] || 'beta';
+/**
+ * List the possible new versions.
+ * Borrowed form version-bump-prompt
+ * https://github.com/BigstickCarpet/version-bump-prompt/blob/master/lib/index.js#L50
+ * @param  {string} baseVer     Initial version.
+ * @return {object}             Available versions.
+ */
+function versionInfo(baseVer = '0.1.0') {
+    let current = new semver(baseVer);
+    let identifier = current.prerelease[0] || 'beta';
 
     return {
         current: current.version,
@@ -78,7 +83,36 @@ var versionInfo = function(version) {
         nextPrePatch: semver.inc(current.version, 'prepatch', identifier),
         nextPreRelease: semver.inc(current.version, 'prerelease', identifier)
     };
-};
+}
+
+
+/**
+ * Promise-base `writeFile` function.
+ * @param  {string} targetPath  File path.
+ * @param  {string} content     File content.
+ * @return {promise}            Promise to write the given file.
+ */
+function writeFileP(targetPath, content) {
+    let fileName = targetPath.split('/').pop();
+
+    if (targetPath.endsWith('.json')) {
+        content = JSON.stringify(content, null, 4);
+    }
+
+    return new Promise((resolve, reject) => {
+
+        fs.writeFile(targetPath, content, (err) => {
+            /* eslint-disable curly */
+            if (err) reject(err);
+            /* eslint-enable curly */
+
+            resolve({
+                fullPath: targetPath,
+                fileName
+            });
+        });
+    });
+}
 
 
 // var git = function git(cmd, options) {
@@ -240,8 +274,24 @@ var versionInfo = function(version) {
 // };
 
 
+/**
+ * Function to manage errors.
+ * @param  {object} reason  Reasons.
+ * @return {object}         Error message.
+ */
+function catchError(reason) {
+    console.error(
+        chalk.red(`\n${ figures.cross } ${ reason }`)
+    );
+
+    process.exit(1);
+}
+
+
 module.exports = {
     getCurrentBranch,
     checkOverallStatus,
-    versionInfo
+    versionInfo,
+    catchError,
+    writeFileP
 };
