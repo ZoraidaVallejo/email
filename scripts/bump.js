9c52333887e196ea8e8026b76f1cb8a332bbcb5c
Diego Miguel <digznav@gmail.com>
2017-05-15 15:22:10 -0500

    :bug: Fix target branch

--- scripts/bump.js
+++ scripts/bump.js
@@ -29,7 +29,7 @@ const PKG = require(path.join(cwd, 'package.json'));
 // ----------------------------------------------------------------------------------------------------------
 
 var versionList = $.versionInfo(PKG.version),
-    targetBranch = 'master',
+    targetBranch = 'develop',
     repoURL = PKG.repository.url.substring(0, PKG.repository.url.indexOf('.git'));
 
 
