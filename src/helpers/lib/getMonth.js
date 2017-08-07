a4dc36b0f1d2a56d6457cc9a5e9370e90b51641f
Priscila Martinez <priscila.mtz.fraire@gmail.com>
2017-08-07 14:46:25 -0500

    :x: Remove conversion files

--- src/helpers/lib/getMonth.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const months = require('months');
-
-module.exports = function getMonth(val) {
-    let _num = parseInt(val) - 1;
-    return months[_num] ? months[_num] : '[Month]';
-};
