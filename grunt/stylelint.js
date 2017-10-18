5b538f3bd38d1bcc761eb912d07f6119b888a923
Diego Miguel <digznav@gmail.com>
2017-10-18 16:22:17 -0500

    📝 oyez fixes

--- grunt/stylelint.js
+++ grunt/stylelint.js
@@ -1,11 +1,11 @@
 const cwd = process.cwd();
 const path = require('path');
 
-module.exports = {
+module.exports = (grunt, { version, paths }) => ({
   options: {
     configFile: path.join(cwd, '.stylelintrc.json'),
     failOnError: false,
     syntax: 'scss'
   },
-  src: '<%= paths.src %>/css/scss/**/*.scss'
-};
+  src: `${paths.src}${!version ? '/css' : ''}/scss/**/*.scss`
+});
