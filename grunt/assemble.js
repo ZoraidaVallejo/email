4e622cc517894af1165f72a06cc991debffa3fda
Diego Miguel <digznav@gmail.com>
2019-02-03 23:03:35 -0600

    Correct position

--- grunt/assemble.js
+++ grunt/assemble.js
@@ -5,11 +5,7 @@ const cwd = process.cwd();
 module.exports = {
   options: {
     layouts: [path.join(cwd, '/common/layouts/*.hbs'), '<%= paths.src %>/layouts/*.hbs'],
-    partials: [
-      path.join(cwd, '/common/partials/**/*.hbs'),
-      path.join(cwd, '/common/ui-components/*.hbs'),
-      '<%= paths.src %>/partials/**/*.hbs'
-    ],
+    partials: [path.join(cwd, '/common/partials/**/*.hbs'), '<%= paths.src %>/partials/**/*.hbs'],
     plugins: 'grunt-assemble-contextual',
     contextual: {
       dest: './temp/'
