2aaa65da4816b63ca09c6e6b2065055b96246116
Diego Miguel <digznav@gmail.com>
2019-02-15 17:43:09 -0600

    More updates

--- grunt/htmlmin.js
+++ grunt/htmlmin.js
@@ -1,17 +1,40 @@
+const files = [
+  {
+    expand: true,
+    flatten: true,
+    src: '<%= paths.dist %>/*.html',
+    dest: '<%= paths.dist %>'
+  }
+];
+
 module.exports = {
+  compressed: {
+    options: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeEmptyAttributes: attrName => attrName === 'style',
+      minifyCSS: {
+        compatibility: {
+          properties: {
+            zeroUnits: false
+          }
+        },
+        level: {
+          1: {
+            removeQuotes: false,
+            optimizeOutline: false
+          }
+        }
+      }
+    },
+    files
+  },
   dist: {
     options: {
       removeComments: true,
       collapseWhitespace: true,
       removeEmptyAttributes: attrName => attrName === 'style'
     },
-    files: [
-      {
-        expand: true,
-        flatten: true,
-        src: '<%= paths.dist %>/*.html',
-        dest: '<%= paths.dist %>'
-      }
-    ]
+    files
   }
 };
