a394c4659aef9a3eac6df021b42ae5abb659354a
Diego Miguel <digznav@gmail.com>
2019-02-23 19:03:23 -0600

    shorthand-font Sass function

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const font = require('css-font');
 const sass = require('node-sass');
 const Eyeglass = require('eyeglass');
 
@@ -16,13 +17,38 @@ module.exports = (grunt, { paths }) => {
       ext: '.css'
     }
   ];
+  // 'shorthand-font($style, $variant, $weight, $stretch, $size, $lineHeight, $family)': (style, variant, weight, stretch, size, lineHeight, family) => {
+  const customFunctions = {
+    'shorthand-font($fontMap)': fontMap => {
+      const config = {
+        style: fontMap.getValue(0).getValue(),
+        variant: fontMap.getValue(1).getValue(),
+        weight: fontMap.getValue(2).getValue(),
+        stretch: fontMap.getValue(3).getValue(),
+        size: fontMap.getValue(4).getValue(),
+        lineHeight: fontMap.getValue(5).getValue()
+      };
+
+      const famHolder = fontMap.getValue(6);
+      const family = [];
+
+      for (let idx = 0; idx < famHolder.getLength(); idx += 1) {
+        family.push(famHolder.getValue(idx).getValue());
+      }
+
+      config.family = family;
+
+      return sass.types.String(font.stringify(config));
+    }
+  };
 
   return {
     dist: {
       options: Eyeglass({
         implementation: sass,
         outputStyle: 'compressed',
-        includePaths
+        includePaths,
+        functions: customFunctions
       }),
       files
     },
@@ -31,7 +57,8 @@ module.exports = (grunt, { paths }) => {
       options: Eyeglass({
         implementation: sass,
         outputStyle: 'expanded',
-        includePaths
+        includePaths,
+        functions: customFunctions
       }),
       files
     },
