ccf62612ccc1ac8bcb15c06607615562de24d4f6
Diego Miguel <digznav@gmail.com>
2019-02-24 20:48:36 -0600

    Sort media queries

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -1,11 +1,28 @@
+const MQpacker = require('css-mqpacker');
 const Autoprefixer = require('autoprefixer');
 
 module.exports = {
   preview: {
     options: {
       map: false,
-      processors: [Autoprefixer]
+      processors: [
+        Autoprefixer,
+        MQpacker({
+          sort: true
+        })
+      ]
     },
-    src: 'preview/css/preview.css'
+    src: '<%= paths.preview %>/css/preview.css'
+  },
+  source: {
+    options: {
+      map: false,
+      processors: [
+        MQpacker({
+          sort: true
+        })
+      ]
+    },
+    src: '<%= paths.src %>/css/*.css'
   }
 };
