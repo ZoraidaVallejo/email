ba71520390bf77b1b6d42f63f7da8aa5198df1a4
Diego Miguel <digznav@gmail.com>
2019-06-24 10:29:02 -0500

    Dynamic Adjustments for mockups

--- preview/js/index.js
+++ preview/js/index.js
@@ -4,17 +4,63 @@ import initButtons from './init-buttons.js';
 import initOpacitySlider from './init-opacity-slider.js';
 
 window.domready(function ready() {
-  // Reusables
   const templateSelect = document.getElementById('template-select');
   const { hash } = document.location;
   const previewWindow = document.querySelector('iframe');
   const mockupWrapper = document.querySelector('.mockup-mask');
   const mockupImg = mockupWrapper.querySelector('img');
+  const mockupPosChanger = document.querySelector('.mockup-position .position');
 
-  var currentMockup = templateSelect.value.replace('.html', '');
-  // var allMockups = getMockupsNames(templateSelect);
+  var mockupAdjustments = {};
+  var currentMockup;
+  var allMockups = getMockupsNames(templateSelect);
 
-  // resetMockupsPosition();
+  allMockups.forEach(name => {
+    mockupAdjustments[name] = Number(localStorage.getItem(`${name}-adjust`)) || 0;
+  });
+
+  function adjustMockup(img, name, value) {
+    mockupAdjustments[name] = value;
+    // eslint-disable-next-line no-param-reassign
+    img.style.transform = `translateY(${mockupAdjustments[name]}px)`;
+
+    localStorage.setItem(`${name}-adjust`, value);
+  }
+
+  // ==================================================================================================================
+
+  // On change, reload template
+  templateSelect.addEventListener(
+    'change',
+    function reloadTemplate(event) {
+      const element = event.target;
+      const { value } = element;
+      const ms = new Date().getTime(); // We'll timestamp each iframe load for cache-busting
+
+      if (!value) {
+        return;
+      }
+
+      currentMockup = templateSelect.value.replace('.html', '');
+      mockupPosChanger.value = mockupAdjustments[currentMockup]; // Set slider to actual position.
+
+      adjustMockup(mockupImg, currentMockup, mockupAdjustments[currentMockup]);
+
+      previewWindow.src = `${value}?t=${ms}`;
+      document.location.hash = `template:${value}`;
+    },
+    false
+  );
+
+  // Preload selected template from hashed template:
+  if (hash && hash.indexOf('template:') !== -1) {
+    const tpl = hash.split(':')[1];
+    templateSelect.value = tpl;
+
+    triggerEvent(templateSelect, 'change');
+  }
+
+  // ==================================================================================================================
 
   previewWindow.addEventListener('load', function setMockupClasses() {
     var innerDoc = previewWindow.contentDocument;
@@ -35,47 +81,17 @@ window.domready(function ready() {
     });
   });
 
-  const mockupPosChanger = document.querySelector('.mockup-position .position');
-  // TODO:
-  // - Change values to dynamic.
-  // - Get from local storage.
-  var adjustMockup = {
-    'client-newsletter': 22,
-    'jld-newsletter': 5
-  };
+  // ==================================================================================================================
+
+  adjustMockup(mockupImg, currentMockup, mockupAdjustments[currentMockup]);
 
   mockupPosChanger.addEventListener('input', function repositionMockup(event) {
     currentMockup = templateSelect.value.replace('.html', '');
-    adjustMockup[currentMockup] = Number(event.target.value);
 
-    mockupImg.style.transform = `translateY(${adjustMockup[currentMockup]}px)`;
+    adjustMockup(mockupImg, currentMockup, Number(event.target.value));
   });
 
-  // On change, reload template
-  templateSelect.addEventListener(
-    'change',
-    function reloadTemplate(event) {
-      const element = event.target;
-      const { value } = element;
-      const ms = new Date().getTime(); // We'll timestamp each iframe load for cache-busting
-
-      if (!value) {
-        return;
-      }
-
-      // currentMockup = templateSelect.value.replace('.html', '');
-
-      // TODO:
-      // Change localStorage values.
-      // mockupPosChanger.value = adjustMockup[currentMockup]; // Set slider to actual position.
-
-      // resetMockupsPosition();
-
-      previewWindow.src = `${value}?t=${ms}`;
-      document.location.hash = `template:${value}`;
-    },
-    false
-  );
+  // ==================================================================================================================
 
   const mockupOpacityChanger = document.querySelector('.mockup-position .opacity');
 
@@ -85,12 +101,4 @@ window.domready(function ready() {
   const buttons = buttonsWrapper.querySelectorAll('.button');
 
   initButtons(buttonsWrapper, buttons, mockupWrapper);
-
-  // Preload selected template from hashed template:
-  if (hash && hash.indexOf('template:') !== -1) {
-    const tpl = hash.split(':')[1];
-    templateSelect.value = tpl;
-
-    triggerEvent(templateSelect, 'change');
-  }
 });
